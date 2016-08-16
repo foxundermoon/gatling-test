1a56f01e8669b9035d35cce7c33bce3ea36d25f3
foxundermoon <fox@vvfox.com>
2016-08-16 18:41:03 +0800

    add logfile upload simulation

--- src/gatling/scala/logful/server/UploadLogFileLocalSimulation.scala
+++ src/gatling/scala/logful/server/UploadLogFileLocalSimulation.scala
@@ -1,43 +1,47 @@
 package logful.server
 
-import java.io.StringWriter
-import java.nio.charset.StandardCharsets
-import java.util.{Base64, UUID}
+import scala.concurrent.duration._
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import java.util.Base64
+import java.nio.charset.StandardCharsets
+import java.util.UUID
+import java.io.StringWriter
+import com.fasterxml.jackson.databind.ObjectMapper
+import scala.collection.mutable.HashMap
 
-class UploadLogFileSimulation extends Simulation {
+class UploadLogFileLocalSimulation extends Simulation {
 
 	val httpProtocol = http
 		.baseURL("http://192.168.10.56:9610")
 		.inferHtmlResources()
 		.acceptHeader("*/*")
 		.authorizationHeader("bearer 864a39eb-6acb-4816-84c4-66344a46e156")
-		.contentTypeHeader("multipart/form-data; boundary=----------------------507d18cf8b72b879")
+		.contentTypeHeader("multipart/form-data; boundary=------------------------494caab28d95fd06")
 
 	val mapper = new ObjectMapper()
 
-	val users = 10
+	val users = 10000
 
     def genHeader : Map[String, String] = {
-    	val fid = UUID.randomUUID.toString.replace("-", "").toLowerCase
+    	var fid = UUID.randomUUID.toString.replace("-", "").toLowerCase
 
-    	val map = new java.util.HashMap[String, Object]()
+    	var map = new java.util.HashMap[String, Object]()
 	    map.put("uid", "08a0e07727c744cfb1685e5ca9d44890")
 	    map.put("bid", "com.getui.logful.sample")
-	    map.put("sum", "d59e7bd21724afffb4af97ea6b3e46cb")
+	    map.put("sum", "220a3b3399689c37d7badb79b981c81c")
 	    map.put("aid", "57ac4fe045cea34392ac4fbc")
 	    map.put("ver", "0.4.2")
 	    map.put("plt", new Integer(1))
 	    map.put("fid", fid)
 
-	    val writer = new StringWriter
+	    var writer = new StringWriter
 	    mapper.writeValue(writer, map)
-	    val extra = writer.toString()
+	    var extra = writer.toString()
 
-	    val xExtra = Base64.getEncoder.encodeToString(extra.getBytes(StandardCharsets.UTF_8))
+	    var xExtra = Base64.getEncoder.encodeToString(extra.getBytes(StandardCharsets.UTF_8))
 
 	    return Map("Charsert" -> "UTF-8", "X-EXTRA" -> xExtra)
     }
@@ -47,14 +51,15 @@ class UploadLogFileSimulation extends Simulation {
     	headerFeeder(i) = genHeader
     }
 
-    val uri1 = "http://demo.logful.aoapp.com:9600/v1/log/file/upload"
-
-	val scn = scenario("UploadLogFileSimulation")
+	val scn = scenario("UploadLogFileLocalSimulation")
 	    .feed(headerFeeder)
-		.exec(http("uploadLogFile")
+		.exec(http("request_0")
 		.post("/v1/log/file/upload")
 		.headers(Map("Charsert" -> "${Charsert}", "X-EXTRA" -> "${X-EXTRA}"))
-		.body(RawFileBody("RecordedSimulation_0000_request.txt"))
-		.check(status.is(202)))
+		.body( RawFileBody("UploadLogFileLocalSimulation_0000_request.txt"))
+		.check(status.is(202))
+		)
+
+
 	setUp(scn.inject(atOnceUsers(users)).protocols(httpProtocol))
 }
