fc8884117208f03ac88d4ecd15ad7f161ce3e7be
foxundermoon <fox@vvfox.com>
2016-08-31 14:43:55 +0800

    up\date

---
+++ src/gatling/scala/logful/server/DebugLogSimulation.scala
@@ -0,0 +1,26 @@
+package logful.server
+
+import java.util.concurrent.TimeUnit
+
+import io.gatling.core.Predef._
+import logful.server.config.{DebugLogFileReqConfig, VerboseLogFileReqConfig}
+
+import scala.concurrent.duration.FiniteDuration
+
+/**
+  * Created by fox on 8/31/16.
+  */
+class DebugLogSimulation extends Simulation {
+  val from = 100
+  val to = 4000
+  val time = 5 * 60
+
+  val during = new FiniteDuration(time, TimeUnit.SECONDS)
+  val second = during.toSeconds
+  val vd = new DebugLogFileReqConfig((0.6 * ((from + to) * time)).toInt)
+
+  setUp(
+    vd.scn.inject(rampUsersPerSec(from) to to during during).protocols(vd httpProtocol)
+  )
+
+}
---
+++ src/gatling/scala/logful/server/VerboseLogSimulation.scala
@@ -0,0 +1,24 @@
+package logful.server
+
+import java.util.concurrent.TimeUnit
+
+import io.gatling.core.Predef._
+import logful.server.config.{DebugLogFileReqConfig, VerboseLogFileReqConfig}
+
+import scala.concurrent.duration.FiniteDuration
+
+/**
+  * Created by fox on 8/31/16.
+  */
+class VerboseLogSimulation extends Simulation {
+  val from = 100
+  val to = 2000
+  val time = 5 * 60
+
+  val during = new FiniteDuration(time, TimeUnit.SECONDS)
+  val second = during.toSeconds
+  val vc = new VerboseLogFileReqConfig((0.6 * ((from + to) * time)).toInt)
+
+  setUp(vc.scn.inject(rampUsersPerSec(from) to to during during).protocols(vc httpProtocol))
+
+}
